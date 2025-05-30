36a186815953d607c24ef9dc509ca2ccf28eafe0
Ezequiel <eze5025@gmail.com>
2025-05-29 23:07:54 -0300

    estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { InterfazCreacionComponent } from './interfaz-creacion/interfaz-creacion.component';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, InterfazCreacionComponent],
+  imports: [RouterOutlet, NavMenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { InterfazCreacionComponent } from './components/interfaz-creacion/interfaz-creacion.component';
+import { ResultadosComponent } from './components/resultados/resultados.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'nuevo', component: InterfazCreacionComponent},
+    {path: 'resultados', component: ResultadosComponent}
+];
