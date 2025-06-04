83650ba6a6fc7d3a7cf3773ab1ff7badf368bd6b
Ezequiel <eze5025@gmail.com>
2025-06-04 17:49:43 -0300

    cambiado link redireccion

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,11 +9,11 @@ export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(),provideAuth({
         config: {
         authority: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_aViEenrUD',
-        redirectUrl: 'http://localhost:4200',
+        redirectUrl: 'https://www.inquiro.site',
         clientId: '3f5ik94ron6j3lsj1hg5t7504b',
         scope: 'phone openid email',
         responseType: 'code',
-        postLogoutRedirectUri: 'http://localhost:4200'
+        postLogoutRedirectUri: 'https://www.inquiro.site'
       },
       })]
 };
