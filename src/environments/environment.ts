333b12ac76ef61c7fd2ee08c0fe6d007ad4252c2
Ezequiel <eze5025@gmail.com>
2025-09-07 23:08:08 -0300

    Actualizado link cognito y url postLogout en environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     COGNITO_CONFIG: {
-        authority: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_aViEenrUD',
-        redirectUrl: 'https://www.inquiro.site',
-        clientId: '3f5ik94ron6j3lsj1hg5t7504b',
+        authority: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_aRl5eh4uh',
+        redirectUrl: 'https://gestion.inquiro.site',
+        clientId: '7jl1nas9dd8chvba6idon2a856',
         scope: 'phone openid email',
         responseType: 'code',
-        postLogoutRedirectUri: 'https://www.inquiro.site'
+        postLogoutRedirectUri: 'https://gestion.inquiro.site'
     },
     INQUIRO_API_LINK: 'https://api.inquiro.site'
 };
